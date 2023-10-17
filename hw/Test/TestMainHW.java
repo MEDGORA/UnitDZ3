96b43590272504376f74e351ab62da910a80fc4d
Boris Dovbysh <boris.dovbysh@gmail.com>
2023-10-17 16:24:08 +0300

    task done

---
+++ hw/Test/TestMainHW.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+
+public class TestMainHW {
+
+    @Test
+    void IntegerExeption(){
+        assertThat(n.isEqualTo(int));
+    }
+
+    @Test
+    void EvenOddNumberExeption(){
+        assertThat(MainHW.evenOddNumber(4).isEqualeTo(true));
+    }
+
+    @Test
+    void EvenOddNumberExeption(){
+        assertThat(MainHW.evenOddNumber(3).isEqualeTo(false));
+    }
+
+    @Test
+    void IntervalExeption(){
+        assertThat(MainHW.intervsl(4).isEqualeTo(false));
+    }
+
+    @Test
+    void IntervalExeption(){
+        assertThat(MainHW.evenOddNumber(27).isEqualeTo(true));
+    }
+
+}
